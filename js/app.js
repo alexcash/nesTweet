4d2ea40a262a5064782784fd3798ade041e8ce72
Alex Cash <alexcash@me.com>
2012-03-24 14:57:20 -0500

    moved .getJSON out of mousedown event. removed helloworld view. begun work on css.

--- js/app.js
+++ js/app.js
@@ -11,19 +11,14 @@ App.tweetController = Ember.ArrayProxy.create({
     content: []
 });
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-	var url = "http://search.twitter.com/search.json?q=@nest&rpp=5&include_entities=true&result_type=mixed&callback=?";
-	$.getJSON( url, function(data){
-		$(data.results).each(function(){
-			var curTweet = tweet.create();
-			curTweet.user = this.from_user;
-			curTweet.username = this.from_user_name;
-			curTweet.profileImageUrl = this.profile_image_url;
-			curTweet.text = this.text;
-			App.tweetController.pushObject(curTweet);
-		});
+var url = "http://search.twitter.com/search.json?q=nest%20thermostat&rpp=20&include_entities=true&result_type=mixed&callback=?";
+$.getJSON( url, function(data){
+	$(data.results).each(function(){
+		var curTweet = tweet.create();
+		curTweet.user = this.from_user;
+		curTweet.username = this.from_user_name;
+		curTweet.profileImageUrl = this.profile_image_url;
+		curTweet.text = this.text;
+		App.tweetController.pushObject(curTweet);
 	});
-  }
 });
