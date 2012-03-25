3a6675439db76b9887518e6c279b5616b3725161
Alex Cash <alexcash@me.com>
2012-03-25 16:21:43 -0500

    implemented custom search terms

--- js/app.js
+++ js/app.js
@@ -7,18 +7,30 @@ tweet = Ember.Object.extend({
 	text: null,
 });
 
-App.tweetController = Ember.ArrayProxy.create({
-    content: []
+App.searchController = Ember.Object.create({
+	term1: "nest",
+	term2: "thermostat",
+	
+	allTerms: function(){
+		return this.get('term1') + '%20' + this.get('term2');
+	}.property('term1', 'term2'),
 });
 
-var url = "http://search.twitter.com/search.json?q=nest%20thermostat&rpp=20&include_entities=true&result_type=mixed&callback=?";
-$.getJSON( url, function(data){
-	$(data.results).each(function(){
-		var curTweet = tweet.create();
-		curTweet.user = this.from_user;
-		curTweet.username = this.from_user_name;
-		curTweet.profileImageUrl = this.profile_image_url;
-		curTweet.text = this.text;
-		App.tweetController.pushObject(curTweet);
+App.searchController.addObserver("allTerms", function(){
+	App.display.set("content", []);
+	var url = "http://search.twitter.com/search.json?q=" + this.get('allTerms') + "&rpp=20&include_entities=true&result_type=mixed&callback=?";
+	$.getJSON( url, function(data){
+		$(data.results).each(function(){
+			var curTweet = tweet.create();
+			curTweet.user = this.from_user;
+			curTweet.username = this.from_user_name;
+			curTweet.profileImageUrl = this.profile_image_url;
+			curTweet.text = this.text;
+			App.display.pushObject(curTweet);
+		});
 	});
+});
+
+App.display = Ember.ArrayProxy.create({
+	content: [],
 });
